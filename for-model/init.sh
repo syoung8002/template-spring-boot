d69d7b42696293fa0e3385b5e3c17df2a33c8203
Jinyoung Jang <jyjang@uengine.org>
2023-06-06 20:45:57 +0900

    Update init.sh

--- for-model/init.sh
+++ for-model/init.sh
@@ -13,6 +13,10 @@ sudo .././aws/install
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 14.19.0 && nvm use 14.19.0
+
 cd kafka
 docker container rm kafka-kafka-1 kafka-zookeeper-1
 docker-compose up
