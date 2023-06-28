ac6a83f2f27d9cccc14c95e90b0209e5b730d438
kibum0405 <59447401+kibum0405@users.noreply.github.com>
2023-06-28 15:52:07 +0900

    Update vuetify.js

--- for-model/frontend/src/plugins/vuetify.js
+++ for-model/frontend/src/plugins/vuetify.js
@@ -4,4 +4,8 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+        primary: options.rootModel.uiStyle.palette.colors.primary.colorCode
+    }
 });
