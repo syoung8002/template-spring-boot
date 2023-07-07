54980acf4e27a8acd09845d42423c8b065cf549e
kibumpark <top5w5@naver.com>
2023-07-07 11:38:37 +0900

    displayName 테스트

--- for-model/frontend/src/plugins/vuetify.js
+++ for-model/frontend/src/plugins/vuetify.js
@@ -18,11 +18,11 @@ const themeColors = {
 };
 
 const defaultColors = {
-    surface: '#FFFFFF',
-    mainText:'#000000',
-    secondaryText:'#808080',
-    primary: '#1976D2',
-    'primary-darken-1': '#1565C0',
+    surface: '#FFFFFF', // 카드 배경색
+    mainText:'#000000', // 메인 글자 색
+    secondaryText:'#808080', // 서브 글자 색
+    primary: '#1976D2', // 버특 및 상단 바 색상
+    'primary-darken-1': '#1565C0', // 다이얼로그 상단 바 색상
     secondary: '#424242',
     'secondary-darken-1': '#212121',
     info: '#2196F3',
